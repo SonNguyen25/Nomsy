dec049526df4d82b01ea58487c23f75a821cd53d
joshualin1212 <93230081+joshualin1212@users.noreply.github.com>
2025-04-11 01:15:38 -0400

    fixed gradle files
     to use the dependencies from a4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,21 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs.kotlin")
     id("kotlin-kapt")
-    id("com.google.devtools.ksp")
-}
-kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(17))
-    }
-}
-kapt {
-    correctErrorTypes = true
-    javacOptions {
-        // Pass the export flag so that jdk.compiler exports com.sun.tools.javac.main to the unnamed module.
-        option("-J--add-exports=jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED")
-
-    }
-
 }
 
 android {
@@ -34,6 +19,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -44,72 +32,129 @@ android {
                 "proguard-rules.pro"
             )
         }
+
+        debug {
+
+        }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions { jvmTarget = "17" }
-    buildFeatures { compose = true }
-    composeOptions { kotlinCompilerExtensionVersion = "1.5.2" }
-    packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
-    buildToolsVersion = "34.0.0"
-    buildFeatures { viewBinding = true }
-}
-configurations.all {
-    resolutionStrategy {
-        force("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
-dependencies {
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.runtime.android)
+dependencies {
+    implementation("androidx.fragment:fragment-ktx:1.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.camera:camera-camera2:1.4.1")
+    implementation("androidx.camera:camera-lifecycle:1.4.1")
+    implementation("androidx.camera:camera-view:1.4.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.5")
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
+    implementation("com.github.bumptech.glide:compose:1.0.0-beta01")
+    implementation("androidx.compose.material3:material3-android:1.3.1")
+    debugImplementation("androidx.fragment:fragment-testing:1.8.5")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation("androidx.navigation:navigation-testing:2.8.5")
+
+    // jetpack compose
+    val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material:material:1.7.6")
+    implementation("androidx.compose.foundation:foundation:1.7.6")
+    implementation("androidx.compose.runtime:runtime-livedata:1.7.6")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // Room
     val room_version = "2.6.1"
 
-        implementation("androidx.room:room-runtime:$room_version")
-        ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    kapt("androidx.room:room-compiler:$room_version")
+
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
     // optional - Kotlin Extensions and Coroutines support for Room
-        implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
 
-        // optional - Test helpers
-        testImplementation("androidx.room:room-testing:$room_version")
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
 
-        // optional - Paging 3 Integration
-        implementation("androidx.room:room-paging:$room_version")
-    implementation(libs.androidx.runtime.livedata)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    // Compose UI
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    // Runtime
-    implementation(libs.androidx.runtime)
-    // Card
-    implementation(libs.androidx.material)
-    // Preview support
-    implementation(libs.androidx.ui.tooling.preview)
-    debugImplementation(libs.androidx.ui.tooling)
-    //Glide Image
-    implementation(libs.compose)
-    implementation("androidx.compose.ui:ui:<compose_version>")
-    implementation("androidx.compose.material:material:<compose_version>")
-    implementation("androidx.compose.ui:ui-tooling-preview:<compose_version>")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:<version>")
-    implementation("androidx.activity:activity-compose:<version>")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("androidx.paging:paging-runtime:3.1.0")
-    implementation("androidx.paging:paging-compose:1.0.0-alpha14")
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
 
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
 
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
 
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+
+    // Dependencies for local unit tests
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // Dependencies for Android instrumented unit tests
+    androidTestImplementation("junit:junit:4.13.2")
+    androidTestImplementation("org.mockito:mockito-core:3.4.6")
+    androidTestImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    //rxjava
+    implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+    // Because RxAndroid releases are few and far between, it is recommended you also
+    // explicitly depend on RxJava's latest version for bug fixes and new features.
+    // (see https://github.com/ReactiveX/RxJava/releases for latest 3.x.x version)
+    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // paging
+    implementation("androidx.paging:paging-runtime-ktx:3.3.5")
+    implementation("androidx.paging:paging-compose:3.3.5")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,39 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("com.android.library") version "8.1.4" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-        id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
-
+    id("com.android.application") version "8.8.0" apply false
+    id("com.android.library") version "8.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
-
 buildscript {
-
     repositories {
         google()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:8.1.4")
-        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+        classpath("com.android.tools.build:gradle:8.8.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.5")
     }
 }
+
+//
+//
+//plugins {
+//    id("com.android.application") version "8.3.2" apply false
+//    id("com.android.library") version "8.1.4" apply false
+//    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+//        id("com.google.devtools.ksp") version "2.0.21-1.0.27" apply false
+//
+//}
+//
+//buildscript {
+//
+//    repositories {
+//        google()
+//    }
+//    dependencies {
+//        classpath ("com.android.tools.build:gradle:8.1.4")
+//        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+//    }
+//}
+
